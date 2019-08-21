31e2580883b141de0137ef381ffd9af191b0e8b3
Hamza <hamza5839@gmail.com>
2019-08-20 22:10:03 -0700

    commit

--- app/src/main/java/com/example/recycler/MyListAdapter.java
+++ app/src/main/java/com/example/recycler/MyListAdapter.java
@@ -23,7 +23,6 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(listItem);
         return viewHolder;
-        //ggfffyh
     }
 
     @Override
